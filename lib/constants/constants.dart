ffacdf860f5d5d200a9dceae5a566a4ca97e316a
gurkay <g_basyigit@hotmail.com>
2022-01-11 21:21:39 +0300

    add constants

---
+++ lib/constants/constants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const cPrimaryColor = Color(0xFF6F35A5);
+const cPrimaryLightColor = Color(0xFFF1E6FF);
+
+const cTitleScreenWelcome = 'Seçim Robotu';
+const cTitleScreenHome = 'Ana Sayfa';
+const cTitleScreenLogin = 'Giris';
+const cTitleScreenSingUp = 'Kullanici Kayit';
+const cTitleScreenAddCategory = 'Kategori Ekle';
+
+enum LoginStatus {
+  notSignIn,
+  signIn,
+}
---
+++ lib/screens/welcome/screen_welcome.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ScreenWelcome extends StatelessWidget {
+  static String routeName = '/';
+  const ScreenWelcome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
