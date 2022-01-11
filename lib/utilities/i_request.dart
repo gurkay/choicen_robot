14f60d35d3450601cfc809da9f92167643e7a42c
gurkay <g_basyigit@hotmail.com>
2022-01-11 23:11:22 +0300

    add interface class

---
+++ lib/utilities/i_request.dart
@@ -0,0 +1,6 @@
+class IRequest<T> {
+  insert(T data) {}
+  delete(T data) {}
+  update(T data) {}
+  read(T data) {}
+}
---
+++ lib/utilities/i_response.dart
@@ -0,0 +1,6 @@
+class IResponse<T> {
+  doInsert(T data) {}
+  doDelete(T data) {}
+  doUpdate(T data) {}
+  doRead(T data) {}
+}
