bd6c928440260eb08d6ceff887e9775c7b4b6c9f
Gürkay Başyiğit <g_basyigit@hotmail.com>
2022-01-12 17:19:27 +0300

    Update user.dart

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,1 +1,34 @@
-
+class User{
+  int? _userId;
+  String? _userEmail;
+  String? _userPassword;
+  
+  User(
+    this._userEmail,
+    this._userPassword,
+  );
+  
+  User.withUserId(
+    this._userId,
+    this._userEmail,
+    this._userPassword,
+  );
+  
+  int get userId => _userId!;
+  
+  set userEmail(String newUserEmail){
+    if(newUserEmail.length <= 200){
+      this._userEmail = newUserEmail;
+    }
+  }
+  
+  String get userEmail => _userEmail!;
+  
+  set userPassword(String newUserPassword){
+    if(newUserPassword.length <= 200){
+      this._userPassword = newUserPassword;
+    }
+  }
+  
+  String get userPassword => _userPassword;
+}
