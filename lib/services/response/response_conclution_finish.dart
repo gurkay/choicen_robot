a0c326aed3d2fd246cfcadf89ea38705022f8978
gurkay <g_basyigit@hotmail.com>
2022-01-29 12:34:15 +0300

    new codes

---
+++ lib/services/response/response_conclution_finish.dart
@@ -0,0 +1,59 @@
+import 'package:choicen_robot/models/conclution_finish.dart';
+import '../request/request_conclution_finish.dart';
+
+import 'package:choicen_robot/utilities/i_response.dart';
+
+abstract class CallBackConclutionFinish {
+  void onSuccessDoInsertConclutionFinish(ConclutionFinish conclutionFinish);
+  void onSuccessDoListConclutionFinish(List<ConclutionFinish> conclutionFinies);
+  void onSuccessDoDeleteConclutionFinish(int result);
+  void onErrorConclutionFinish(String error);
+}
+
+class ResponseConclutionFinish implements IResponse {
+  final CallBackConclutionFinish _callBackConclutionFinish;
+  ResponseConclutionFinish(this._callBackConclutionFinish);
+  RequestConclutionFinish requestConclutionFinish =
+      new RequestConclutionFinish();
+
+  @override
+  doDelete(dynamic conclutionFinishId) {
+    requestConclutionFinish
+        .delete(conclutionFinishId)
+        .then((value) =>
+            _callBackConclutionFinish.onSuccessDoDeleteConclutionFinish(value))
+        .catchError((onError) => _callBackConclutionFinish
+            .onErrorConclutionFinish(onError.toString()));
+  }
+
+  @override
+  doInsert(dynamic conclutionFinish) {
+    requestConclutionFinish
+        .insert(conclutionFinish)
+        .then((value) =>
+            _callBackConclutionFinish.onSuccessDoInsertConclutionFinish(value))
+        .catchError((onError) => _callBackConclutionFinish
+            .onErrorConclutionFinish(onError.toString()));
+  }
+
+  @override
+  doRead(data) {
+    // TODO: implement doRead
+    throw UnimplementedError();
+  }
+
+  @override
+  doUpdate(data) {
+    // TODO: implement doUpdate
+    throw UnimplementedError();
+  }
+
+  doListConclutionFinish(dynamic categoryId) {
+    requestConclutionFinish
+        .getListConclutionFinishs(categoryId)
+        .then((value) =>
+            _callBackConclutionFinish.onSuccessDoListConclutionFinish(value))
+        .catchError((onError) => _callBackConclutionFinish
+            .onErrorConclutionFinish(onError.toString()));
+  }
+}
